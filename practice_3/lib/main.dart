5b80f906d99c28e4ddf8cf8e478a1243e756f13b
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2024-11-29 18:06:19 +0300

    HW-2 completed.

--- practice_3/lib/main.dart
+++ practice_3/lib/main.dart
@@ -1,125 +1,131 @@
 import 'package:flutter/material.dart';
+import './weather_api/weather_api.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  var resp = await getWeather("Москва");
+  print("Температура воздуха: ${resp.tempC}");
+  print("Ощущается как: ${resp.feelsLikeC}");
+  print("Описание: ${resp.condition}");
+
+  runApp(const WeatherApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const WeatherScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class WeatherScreen extends StatefulWidget {
+  const WeatherScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<WeatherScreen> createState() => _WeatherScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  final TextEditingController _cityController = TextEditingController();
+  String? _cityName;
+  Weather? _weatherData;
+  String? _errorMessage;
+
+
+  Future<void> _getWeatherData(String cityName) async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+       _cityName = cityName;
+      _errorMessage = null;
+      _weatherData = null;
     });
+
+    try {
+      Weather weatherData = await getWeather(cityName);
+      setState(() {
+        _weatherData = weatherData;
+      });
+    }
+    catch(e){
+      setState(() {
+      _errorMessage = "Город не найден.";
+    });
+    }
+
   }
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.black87,
+        title: const Text('Погода', style: TextStyle(color: Color.fromARGB(255, 173, 163, 228)),),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.blue, Colors.purple])
+        ),
+        padding: const EdgeInsets.all(32.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            TextField(
+              controller: _cityController,
+              decoration: InputDecoration(
+                hintStyle: TextStyle(color: Colors.white70),
+                hintText: 'Введите название города',
+                prefixIcon: IconButton(icon: const Icon(Icons.search), onPressed: (){_getWeatherData(_cityController.text);})
+              ),
+              onSubmitted: (value) {
+                _getWeatherData(value);
+              },
+              style: TextStyle(color: Colors.white70),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const SizedBox(height: 50),
+            
+            if (_weatherData != null)
+              Card(shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0)
+              ),
+              color: Colors.grey.withOpacity(0.1), 
+              child: Padding(padding: const EdgeInsets.all(32.0),
+                child: Column(
+                  children: [
+                    Text('Город: $_cityName', style: const TextStyle(fontSize: 35, color: Color.fromARGB(255, 173, 163, 228))),
+                    Text(
+                        'Температура: ${_weatherData!.tempC}°C',
+                       style: const TextStyle(fontSize: 21, color: Colors.white70)),
+                    Text(
+                        'Ощущается как: ${_weatherData!.feelsLikeC}°C',
+                        style: const TextStyle(fontSize: 21, color: Colors.white70)),
+                   Text(
+                        'Условия: ${_weatherData!.condition}',
+                       style: const TextStyle(fontSize: 21, color: Colors.white70)),
+                    Image.network( 'http://cdn.weatherapi.com/weather/64x64/day/${_weatherData!.icon.split('/').last}')
+                  ],
+              ),))
+              ,
+
+            if (_errorMessage != null)
+              Center(
+                child: Text(
+                _errorMessage!,
+                style: const TextStyle(fontSize: 20, color: Color.fromARGB(255, 255, 255, 255)),
+              ),
+              )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+  
---
+++ practice_3/lib/weather_api/weather_api.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const String url = 'http://api.weatherapi.com/v1/current.json';
+const String apiKey="3c573eef0b08472c891183328240810";
+
+
+class Weather {
+  final double tempC;
+  final double feelsLikeC;
+
+  final String condition;
+
+  final dynamic icon;
+
+  const Weather(this.tempC, this.feelsLikeC, this.condition, this.icon);
+}
+
+
+Future<Weather> getWeather(String city) async {
+  var response = await http.get(Uri.parse('$url?&q=$city&key=$apiKey'));
+
+  if (response.statusCode == 200){
+    var rJson = jsonDecode(response.body);
+
+    return Weather(rJson['current']['temp_c'], rJson['current']['feelslike_c'], rJson['current']['condition']['text'], rJson['current']['condition']['icon']);
+  }
+
+  else {
+    throw Exception("City was not found.");
+  }
+  
+}
